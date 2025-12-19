a3a4ef9f9e3adfe58a03bae13464ed2ceb8c0630
Jirakid Aiadhet <167008371+TOEYJIRAKID@users.noreply.github.com>
2025-12-19 18:50:57 +0700

    update file

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -32,6 +32,9 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+
+    // await new Promise((resolve) => setTimeout(resolve, 1500));
+
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -159,7 +162,8 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
